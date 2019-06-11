5142e22310784f0c63c17d3c2b3f5388fa0a041e
Jennifer Omena <jennifer.omena@m4u.com.br>
2019-06-11 11:12:22 -0300

    mudança de nome de funções

--- src/main.js
+++ src/main.js
@@ -1,109 +1,109 @@
-function Feminino(idade, calculo) {
+function calculoFeminino(idade, indice) {
     let resultado
 
     if (idade == '10') {
-        if (calculo <= 14,22)
+        if (indice <= 14,22)
             resultado = "Abaixo do peso"
-        else if (calculo >= 14.23 && calculo <= 20.18)
+        else if (indice >= 14.23 && indice <= 20.18)
             resultado = "Peso adequado"
         
-        else if (calculo >= 20.19)
+        else if (indice >= 20.19)
             resultado = "Sobrepeso"
     }
     else if (idade == '11') {
-        if (calculo <= 14,59)
+        if (indice <= 14,59)
             resultado = "Abaixo do peso"
-        else if (calculo >= 14.6 && calculo <= 21.17)
+        else if (indice >= 14.6 && indice <= 21.17)
             resultado = "Peso adequado"
-        else if (calculo >= 21.18)
+        else if (indice >= 21.18)
             resultado = "Sobrepeso"
     }
     else if (idade == '12') {
-        if (calculo <= 14.97)
+        if (indice <= 14.97)
             resultado = "Abaixo do peso"
-        else if (calculo >= 14.98 && calculo <= 22.16)
+        else if (indice >= 14.98 && indice <= 22.16)
             resultado = "Peso adequado"
-        else if (calculo >= 22.17)
+        else if (indice >= 22.17)
             resultado = "Sobrepeso"
     }
     else if (idade == '13') {
-        if (calculo <= 15.35)
+        if (indice <= 15.35)
             resultado = "Abaixo do peso"
-        else if (calculo >= 15.36 && calculo <= 23.07)
+        else if (indice >= 15.36 && indice <= 23.07)
             resultado = "Peso adequado"
-        else if (calculo >= 23.08)
+        else if (indice >= 23.08)
             resultado = "Sobrepeso"
     }
     else if (idade == '14') {
-        if (calculo <= 15.66)
+        if (indice <= 15.66)
             resultado = "Abaixo do peso"
-        else if (calculo >= 15.67 && calculo <= 23.87)
+        else if (indice >= 15.67 && indice <= 23.87)
             resultado = "Peso adequado"
-        else if (calculo >= 23.88)
+        else if (indice >= 23.88)
             resultado = "Sobrepeso"
     }
     else if (idade == '15') {
-        if (calculo <= 16)
+        if (indice <= 16)
             resultado = "Abaixo do peso"
-        else if (calculo >= 16.01 && calculo <= 24.28)
+        else if (indice >= 16.01 && indice <= 24.28)
             resultado = "Peso adequado"
-        else if (calculo >= 24.29)
+        else if (indice >= 24.29)
             resultado = "Sobrepeso"
     }
     else if (idade == '16') {
-        if (calculo <= 16.36)
+        if (indice <= 16.36)
             resultado = "Abaixo do peso"
-        else if (calculo >= 16.37 && calculo <= 24.73)
+        else if (indice >= 16.37 && indice <= 24.73)
             resultado = "Peso adequado"
-        else if (calculo >= 24.74)
+        else if (indice >= 24.74)
             resultado = "Sobrepeso"
     }
     else if (idade == '17') {
-        if (calculo <= 16.58)
+        if (indice <= 16.58)
             resultado = "Abaixo do peso"
-        else if (calculo >= 16.59 && calculo <= 25.22)
+        else if (indice >= 16.59 && indice <= 25.22)
             resultado = "Peso adequado"
-        else if (calculo >= 25.23)
+        else if (indice >= 25.23)
             resultado = "Sobrepeso"
     }
     else if (idade == '18') {
-        if (calculo <= 16.7)
+        if (indice <= 16.7)
             resultado = "Abaixo do peso"
-        else if (calculo >= 16.71 && calculo <= 25.55)
+        else if (indice >= 16.71 && indice <= 25.55)
             resultado = "Peso adequado"
-        else if (calculo >= 25.56)
+        else if (indice >= 25.56)
             resultado = "Sobrepeso"
     }
     else if (idade == '19') {
-        if (calculo <= 16.86)
+        if (indice <= 16.86)
             resultado = "Abaixo do peso"
-        else if (calculo >= 16.87 && calculo <= 25.84)
+        else if (indice >= 16.87 && indice <= 25.84)
             resultado = "Peso adequado"
-        else if (calculo >= 25.85)
+        else if (indice >= 25.85)
             resultado = "Sobrepeso"
     }
     else if ((idade>=20) && (idade<=59)){
-        if (calculo < 18.5)
+        if (indice < 18.5)
             resultado = "Abaixo do peso"
-        else if (calculo > 18.5 && calculo <25)
+        else if (indice > 18.5 && indice <25)
             resultado = "Peso adequado"
-        else if (calculo >=25 && calculo < 30)
+        else if (indice >=25 && indice < 30)
             resultado = "Sobrepeso"
-        else if (calculo >= 30 && calculo <= 34.9)
+        else if (indice >= 30 && indice <= 34.9)
             resultado= "Obesidade grau 1"
-        else if (calculo >= 35 && calculo <= 39.9)
+        else if (indice >= 35 && indice <= 39.9)
             resultado= "Obesidade grau 2"
-        else if (calculo >= 35 && calculo <= 39.9) 
+        else if (indice >= 35 && indice <= 39.9) 
             resultado= "Obesidade grau 2"
-        else if (calculo >= 40)
+        else if (indice >= 40)
             resultado= "Obesidade grau 3"
     }
     else if (idade >= 60){
-        if (calculo <= 22)
+        if (indice <= 22)
             resultado = "Abaixo do peso"
-        else if (calculo > 22 && calculo < 27)
+        else if (indice > 22 && indice < 27)
             resultado = "Peso adequado"
-        else if (calculo >= 27)
+        else if (indice >= 27)
             resultado = "Sobrepeso"
     }
 
@@ -111,125 +111,125 @@ function Feminino(idade, calculo) {
     
 }
 
-function Masculino(idade, calculo) {
+function calculoMasculino(idade, indice) {
     if (idade == '10') {
-        if (calculo <= 14,41)
+        if (indice <= 14,41)
             resultado = "Abaixo do peso"
-        else if (calculo >= 14.42 && calculo <= 19.5)
+        else if (indice >= 14.42 && indice <= 19.5)
             resultado = "Peso adequado"
-        else if (calculo >= 19.6)
+        else if (indice >= 19.6)
             resultado = "Sobrepeso"
     }
     else if (idade == '11') {
-        if (calculo <= 14,82)
+        if (indice <= 14,82)
             resultado = "Abaixo do peso"
-        else if (calculo >= 14.83 && calculo <= 20.34)
+        else if (indice >= 14.83 && indice <= 20.34)
             resultado = "Peso adequado"
-        else if (calculo >= 20.35)
+        else if (indice >= 20.35)
             resultado = "Sobrepeso"
     }
     else if (idade == '12') {
-        if (calculo <= 15.23)
+        if (indice <= 15.23)
             resultado = "Abaixo do peso"
-        else if (calculo >= 15.24 && calculo <= 21.11)
+        else if (indice >= 15.24 && indice <= 21.11)
             resultado = "Peso adequado"
-        else if (calculo >= 21.12)
+        else if (indice >= 21.12)
             resultado = "Sobrepeso"
     }
     else if (idade == '13') {
-        if (calculo <= 15.72)
+        if (indice <= 15.72)
             resultado = "Abaixo do peso"
-        else if (calculo >= 15.73 && calculo <= 21.92)
+        else if (indice >= 15.73 && indice <= 21.92)
             resultado = "Peso adequado"
-        else if (calculo >= 21.93)
+        else if (indice >= 21.93)
             resultado = "Sobrepeso"
     }
     else if (idade == '14') {
-        if (calculo <= 16.17)
+        if (indice <= 16.17)
             resultado = "Abaixo do peso"
-        else if (calculo >= 16.18 && calculo <= 22.76)
+        else if (indice >= 16.18 && indice <= 22.76)
             resultado = "Peso adequado"
-        else if (calculo >= 22.77)
+        else if (indice >= 22.77)
             resultado = "Sobrepeso"
     }
     else if (idade == '15') {
-        if (calculo <= 16.58)
+        if (indice <= 16.58)
             resultado = "Abaixo do peso"
-        else if (calculo >= 16.59 && calculo <= 23.62)
+        else if (indice >= 16.59 && indice <= 23.62)
             resultado = "Peso adequado"
-        else if (calculo >= 23.63)
+        else if (indice >= 23.63)
             resultado = "Sobrepeso"
     }
     else if (idade == '16') {
-        if (calculo <= 17)
+        if (indice <= 17)
             resultado = "Abaixo do peso"
-        else if (calculo >= 17.01 && calculo <= 24.44)
+        else if (indice >= 17.01 && indice <= 24.44)
             resultado = "Peso adequado"
-        else if (calculo >= 24.45)
+        else if (indice >= 24.45)
             resultado = "Sobrepeso"
     }
     else if (idade == '17') {
-        if (calculo <= 17.3)
+        if (indice <= 17.3)
             resultado = "Abaixo do peso"
-        else if (calculo >= 17.31 && calculo <= 25.27)
+        else if (indice >= 17.31 && indice <= 25.27)
             resultado = "Peso adequado"
-        else if (calculo >= 25.28)
+        else if (indice >= 25.28)
             resultado = "Sobrepeso"
     }
     else if (idade == '18') {
-        if (calculo <= 17.53)
+        if (indice <= 17.53)
             resultado = "Abaixo do peso"
-        else if (calculo >= 17.54 && calculo <= 25.94)
+        else if (indice >= 17.54 && indice <= 25.94)
             resultado = "Peso adequado"
-        else if (calculo >= 25.95)
+        else if (indice >= 25.95)
             resultado = "Sobrepeso"
     }
     else if (idade == '19') {
-        if (calculo <= 17.79)
+        if (indice <= 17.79)
             resultado = "Abaixo do peso"
-        else if (calculo >= 17.8 && calculo <= 26.35)
+        else if (indice >= 17.8 && indice <= 26.35)
             resultado = "Peso adequado"
-        else if (calculo >= 26.36)
+        else if (indice >= 26.36)
             resultado = "Sobrepeso"
     }
     else if ((idade >= 20) && (idade <= 59)){
-        if (calculo < 18.5)
+        if (indice < 18.5)
             resultado = "Abaixo do peso"
-        else if (calculo > 18.5 && calculo < 25)
+        else if (indice > 18.5 && indice < 25)
             resultado = "Peso adequado"
-        else if (calculo >= 25 && calculo < 30)
+        else if (indice >= 25 && indice < 30)
             resultado = "Sobrepeso"
-        else if (calculo >= 30 && calculo <= 34.9)
+        else if (indice >= 30 && indice <= 34.9)
             resultado= "Obesidade grau 1"
-        else if (calculo >= 35 && calculo <= 39.9)
+        else if (indice >= 35 && indice <= 39.9)
             resultado= "Obesidade grau 2"
-        else if (calculo >= 35 && calculo <= 39.9) 
+        else if (indice >= 35 && indice <= 39.9) 
             resultado= "Obesidade grau 2"
-        else if (calculo >= 40)
+        else if (indice >= 40)
         resultado= "Obesidade grau 3"
     }
     else if (idade >= 60){
-        if (calculo <= 22)
+        if (indice <= 22)
             resultado = "Abaixo do peso"
-        else if (calculo > 22 && calculo < 27)
+        else if (indice > 22 && indice < 27)
             resultado = "Peso adequado"
-        else if (calculo >= 27)
+        else if (indice >= 27)
             resultado = "Sobrepeso"
     }
     return resultado
 }
 
-function calcular(indice, feminino, idade) {
+function calcular(indice, isFeminino, idade) {
     let resultado
 
 
     if (Number.isNaN(indice) || indice <= 0) {
         resultado= "Valores inválidos!"
     } else {
-        if (feminino) {
-            resultado = Feminino(idade, indice)
+        if (isFeminino) {
+            resultado = calculoFeminino(idade, indice)
         } else {
-            resultado = Masculino(idade, indice)
+            resultado = calculoMasculino(idade, indice)
         }
         resultado = resultado + ", com indice " + indice.toFixed(2)
     }
